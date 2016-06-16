dad2418843d88caa86c7d97f04c0f1f9e5ef3521
Aleisha Nelson <nelson.aleisha@gmail.com>
2016-06-16 10:50:51 -0700

    initial commit

---
+++ index.php
@@ -0,0 +1,54 @@
+
+
+
+
+<?php
+
+//create connection    (host, username, password, dbname)
+$conn = mysqli_connect('mysql.wminnovation.xyz', 'aleisha_walmart', 'Sharkey001', 'iamerica_db');
+
+//check connection
+if ($conn)
+  { 
+  print "<br> Connection to the database successful";
+}
+else {
+ 	$err = mysqli_error; 
+	print "<br> Connection Failed";
+	print htmlentities($err['message']);
+	exit;
+  } 
+
+$sql ='SELECT * from GeoNotifs';
+//$result = mysqli_query($con, $sql);
+
+$result =  mysqli_real_query($con, $sql);
+
+$num_columns = mysqli_field_count($conn);
+
+echo "<BR/>";
+echo "</TR><TH>-- Obtaining results<TH>";
+
+// start results formatting
+echo "<TABLE BORDER=1 padding=10px>";
+echo "<TR><TH>StoreID</TH><TH>Notifcation Message</TH><TH>URI</TH></TH>";
+
+while (mysqli_fetch_field($result)){
+echo "<TR>";
+ for ($i = 1; $i <= $num_columns; $i++) {
+    $column_value = mysqli_fetch_object($sql_statement,$i);
+    echo "<TD>$column_value</TD>";
+ }
+echo "</TR>";
+}
+echo "</TABLE>";
+
+//free resources and  close connection
+mysqli_free_result($result);
+mysqli_close($conn);
+
+
+
+
+?>
+
